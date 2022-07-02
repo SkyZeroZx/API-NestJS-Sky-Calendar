41341980fd4804ddb0b14a9302ff7476c4a0a902
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2022-07-01 22:03:37 -0500

    Fixed Deploy

--- src/config/webAuthn.ts
+++ src/config/webAuthn.ts
@@ -7,7 +7,7 @@ import { User } from 'src/user/entities/user.entity';
 // Human-readable title for your website
 const rpName = 'Sky Calendar App';
 // A unique identifier for your website
-const rpID = 'kikesport.com.pe';
+const rpID = 'sky-calendar.herokuapp.com';
 
 const rpIDArray = [
   'localhost',
